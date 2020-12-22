95332d137269b7a18d7bb68d65fa09593e463d3d
ivanrenardo <ivanrenardo@gmail.com>
2020-12-22 18:59:42 +0700

    gas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world. Hallo wandi
+          Hello world. Hallo wandi test
         </p>
         <a
           className="App-link"
